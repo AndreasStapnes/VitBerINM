8bb5932d739eee2d2b92f4986e3c70a0dfd9cb50
Andreas Stapnes <projstapnes@gmail.com>
2021-04-20 14:36:54 +0200

    kommenterte deler av stepMethods

--- stepMethods.py
+++ stepMethods.py
@@ -6,6 +6,48 @@ import numpy as np
 
 
 class stepMethods:
+    '''
+        Dette er en klasse hvilket i grunnen opptrer som et namespace.
+        Instansiering av stepMethods-objekter her vil skape all verdens problemer, men dette ignoreres
+
+        Implementerer her de numeriske metodene
+        på en systematisk metode for å forenkle bruk
+        i rutine-klassen. Alle metodene er jit-akselerert
+        og vil videre anvendes i routine sin execute-prosedyre
+        (hvilket også vil jit-akselereres avhengig av om man spesifiserer dette ved instansiering)
+        '''
+
+    '''
+    Alle metodene er implementert på samme form. Dette vil si at de tar inn
+    en diff-liknings-funksjon f : y'=f(t,y),
+    start-y-verdi (y_n), et start-tidspunkt (t_n), og en steglengde h
+    Metodene returnerer neste y-verdi (y_(n+1)).
+
+    (Selv om parameteren t_n strengt talt ikke er nødvendig, er dette implementert
+    for å generalisere koden)
+
+    Ett unntak fra denne regelen er at Kahans metode ikke tar inn en funksjon, men
+    en tuppel matrixRepresentation hvilket består av matrisene a,b,c og formen (a,b,c) 
+    slik angitt i oppgaveteksten. Disse representerer funksjonen f, og vil brukes for å løse 
+    for y_(n+1). Når man bruker Kahans metode vil man ved instansiering av route bruke
+    (a,b,c) i stedet for function. Ingen andre steder enn i disse stepMethods-metodene vil
+    innholdet av denne parameteren ha noe å si (den passeres rundt med den tolking at den
+    er et objekt hvilket kan brukes til å beregne y_n+1 fra y_n)
+
+    En annen ting som er verd å merke er at dette namespacet inneholder en dictionary
+    hvilket beskriver om hver metode baserer seg på en 'rollover', dvs, om et
+    tidssteg baserer seg på data fra det forrige. Alle metodene som har rollover-verdi
+    satt til True kreves det at metodene tar inn et argument 'rollover' hvilket
+    inneholder disse data. Dessuten må de ha en 'metodenavn'InitialRollover-metode
+    for å beregne rollover-data for starten (oftest hvor man gjør en ekstra funk-
+    sjons-evaluering, som i StV). Metoder med spesifisiert rollover må også returnere en ekstra 
+    rollover variabel til bruk i neste tidssteg
+    '''
+
+    '''
+               RK4 implementerer den 'berømte' runge-kutta-metoden
+               hvilket har orden 4. Fremgangsmåten er slik beskrevet i oppgaven
+       '''
     @jit(nopython=True)
     def RK4(function, yValues, t, h):
         dim = len(yValues)
@@ -17,6 +59,10 @@ class stepMethods:
         yNext = yValues + h / 6 * (Fs[0,:] + 2 * Fs[1,:] + 2 * Fs[2,:] + Fs[3,:])
         return yNext
 
+    '''
+                BS3 implementerer en Bogacki-Shampine-metode av orden 3. 
+                Fremgangsmåten er slik beskrevet i oppgaven
+    '''
     @jit(nopython=True)
     def BS3(function, yValues, t, h):
         dim = len(yValues)
@@ -27,6 +73,10 @@ class stepMethods:
         yNext = yValues + h / 9 * (Fs[0,:] * 2 + Fs[1,:] * 3 + Fs[2,:] * 4)
         return yNext
 
+    '''
+                Kah implementerer Kahans metode med orden 2
+                slik spesifisert i oppgaven. 
+    '''
     @jit(nopython=True)
     def Kah(matrixRepresentation, yvalues, t, h):
         a,b,c = matrixRepresentation
@@ -38,6 +88,13 @@ class stepMethods:
         B = np.identity(n) + 1/2*h*b
         return np.linalg.solve(A, B@yvalues + c)
 
+    '''
+                StV implementerer en Störmer-Verlet-metode
+                av orden 2. Merk at denne også tar inn rollover.
+                Under er det også definert en initialRollover-metode
+                hvilket produserer den første rollover-verdien til bruk
+                i første funksjons-kjøring.
+    '''
     @jit(nopython=True)
     def StV(function, yValues, t, h, rollover):
         n = len(yValues)//2
@@ -64,17 +121,33 @@ class stepMethods:
 
 class routine:
     def __init__(self, **kwargs):
-        '''
+        ''''
         :param kwargs:
             (float) tInit, tFinal                               | default: tInit=0, tFinal=1
             (float) ordinaryStepLen eller (int) steps           | default: ordinaryStepLen=1
             (string) method {RK4, BS3, Ka2, SV2}                | default: RK4
             (ndim float) y0                                     | default: 0-vec
-            (f : t,y->(ndim float)) f                           | default: 0-function
+            (objekt hvilket representerer f:y'=f) f             | default: 0-function
+
+            funksjonalitets-parametre:
+            (bool) timeline
+                Hvorvidt hver y-verdi i kjøring skal lagres
+            (int) timelineJumps
+                Antall verdier i timeline man skipper (+1) før man lagrer en ny verdi
+            (bool) nopythonExe
+                Hvorvidt execution-delen skal kompileres med jit-nopython (i tillegg til funksjonsevalueringer)
+            (bool) savePlaneCuts
+                Hvordvidt alle poincare-kutt med de spesifiserte plan for instansen (lagret i instans.planes)
+                skal lagres i en liste, hvilket omsider returneres ved slutt av instans.run()
+
         '''
+
+        #I de kommende linjene lagres alle instans-variablene
         self.tInit = kwargs.get("tInit", 0)
         self.tFinal = kwargs.get("tFinal", 1)
 
+        #Avhengig om stepAmtBased/step-Amount-based eller step-length-based vil det lagres antall steps eller steg-lengde
+        #Dette valget vedvarer i execution-metoden
         self.stepAmtBased = "steps" in kwargs
         if  (self.stepAmtBased):  self.steps=kwargs.get("steps")
         else                   :  self.ordinaryStepLen = kwargs.get("ordinaryStepLen", 1)
@@ -84,40 +157,48 @@ class routine:
         self.saveTimeline=kwargs.get('timeline', False)
         self.timelineJumps=kwargs.get('timelineJumps', 1)
 
-        self.method = None
+        self.execution = None
         self.nopython=kwargs.get('nopythonExe', False)
 
         self.savePlaneCuts = kwargs.get("savePlaneCuts", False)
-        self.planes = []
+        self.planes = [] #Array av hyperplan aktuelle for poincare-kutt.
 
         self.methodName = kwargs.get("method", "RK4")
         self.function = kwargs.get("f")
 
+    #run er funksjonen en bruker når en ønsker å kjøre rutinen spesifisert av input-parametrene
+    #Den kaller på _load hvilket forbereder kjøring. Deretter utfører den execution assosiert med instansen.
+    #Resultatet fra denne blir returnert av run.
     def run(self):
         self._load()
-        return self.method()
+        return self.execution()
 
+    #Funksjon som ikke skal kalles utenfra. _load forbereder kjøring av prosedyren, deriblant med å (avhengig av valg) jit-kompilere prosedyren
     def _load(self, timeline=False):
-        tInterval = self.tFinal-self.tInit
-        tInit = self.tInit
+        tInterval = self.tFinal-self.tInit          #Definerer lengde på t-intervallet
+        tInit = self.tInit                          #Henter ut start-tidspunkt
         normalSteps = int(np.floor(tInterval/self.ordinaryStepLen)) if not self.stepAmtBased else self.steps
-        endStep = not self.stepAmtBased
-        totalSteps = normalSteps + endStep
-        totalPoints = totalSteps + 1
+                                                    #NormalSteps er antall steg hvilket vil utføres med
+        endStep = not self.stepAmtBased             #'vanlig steglengde'. Dersom prosedyren er step-length-basert,
+                                                    #legges det også til et sluttsteg hvis eksistens er beskrevet av bool endStep
+        totalSteps = normalSteps + endStep          #Variabel hvilket beskriver totalt antall steg prosedyren baseres på
+        totalPoints = totalSteps + 1                #Variabel hvilket beskriver totalt antall y-verdier som prosedyren gjennomgår
         stepLen = float(self.ordinaryStepLen if not self.stepAmtBased else tInterval/self.steps)
-        methodYInitial = self.y0
+                                                    #stepLen angir lengden på de normale (ikke nødv. endstep) stegene under prosedyren
+        methodYInitial = self.y0                    #Henter ut start-verdi
 
         if(self.methodName in stepMethods.__dict__): method = stepMethods.__dict__[self.methodName]
         else: raise Exception("No applicable method found for gived method-name")
+                                                    #Henter ut den aktuelle numeriske utviklingsmetoden method fra stepMethods
 
-        function = self.function
+        function = self.function                    #Henter funksjonen fra instansen
 
 
-        saveTimeline = self.saveTimeline
+        saveTimeline = self.saveTimeline            #Henter div valgfrie funksjonalitets-parametre som er angitt i konstruktøren
         savePlaneCuts = self.savePlaneCuts
 
 
-        methodHasRollover = stepMethods.hasRollover.get(self.methodName)
+        methodHasRollover = stepMethods.hasRollover.get(self.methodName) # Henter om den aktuelle utviklingsmetoden inkluderer rollover (baserer seg på forrgige kjøring)
         rolloverInit = stepMethods.__dict__[self.methodName + "InitialRollover"](function, methodYInitial, tInit, stepLen) if methodHasRollover else None
 
         dimension = len(methodYInitial)
@@ -216,4 +297,4 @@ class routine:
             return ((arr, times, cutpoints) if savePlaneCuts else (arr, times)) \
                                        if saveTimeline else \
                             ((y, cutpoints) if savePlaneCuts else y)
-        self.method = execute
+        self.execution = execute
