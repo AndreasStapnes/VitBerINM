469efa793fa13562015497fcaa86692a6e6bae1f
Andreas Stapnes <projstapnes@gmail.com>
2021-04-26 16:51:26 +0200

    Konstruerte sideprosjekt for enkel numerisk integrasjon med et stort utvalg metoder

---
+++ simpleSolutions.py
@@ -0,0 +1,20 @@
+from numba import jit
+import numpy as np
+import matplotlib.pyplot as plt
+from scenario import *
+from stepMethods import *
+
+@jit(nopython=True)
+def f(t, y, F):
+    F[:] = np.array([t ** 3 / y[0] ** 2])
+
+
+rout = routine(f=f, tInit=0, tFinal=1, y0=np.array([1]), steps=4, method="Mid", timeline=True)
+ys, t = rout.run()
+
+
+
+ys = ys.T
+plt.plot(t, ys[0])
+print(ys[0])
+plt.show()
